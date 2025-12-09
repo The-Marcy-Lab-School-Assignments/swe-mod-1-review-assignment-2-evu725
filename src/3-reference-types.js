13dd9e3e588c93eb70265d53e7852e84da9bc2b6
Emily Vu <evu725@gmail.com>
2025-12-08 19:46:07 -0500

    completed coding problems 1-5

--- src/3-reference-types.js
+++ src/3-reference-types.js
@@ -4,14 +4,16 @@ const team1 = {
   losses: 20
 };
 
-const team2 = team1;
+const team2 = { ...team1 };
 team2.wins = 50;
 team2.losses = 15;
 
+console.log(team1.wins);
+console.log(team2.wins);
 /* 
 Part A: What will team1.wins and team1.losses be after this code runs? Write your prediction as a comment below:
 
-- Prediction:
+- Prediction: team1.wins: 50 and team1.losses: 15
 
 Part B: Fix the code so that team2 is a TRUE COPY of team1. After making your fix, changing team2 should NOT affect team1.
 Part C: Verify your fix by logging both team1.wins (should still be 45) and team2.wins (should be 50)
